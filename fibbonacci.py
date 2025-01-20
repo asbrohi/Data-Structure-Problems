9a4a91ccf2a0fdc51e12d308a6166ec17874a070
Abdul Sattar <asattar.bscsses21@iba-suk.edu.pk>
2025-01-20 14:19:52 +0500

    seoond commit

--- fibbonacci.py
+++ fibbonacci.py
@@ -1,4 +1,16 @@
-num = int(input("Enter the number: "))
-
-def fibbonacci():
+def fibbonacci(number):
+    f0  =  0
+    f1  =  1
+    if number == 1:
+        print(f0)
+    elif number >= 2:
+        print(f0,f1)
+    f3 = 0
+    for i in range (2, (number)):
+        f3 = f0 +f1
+        print(f3, end=" ")
+        f0 = f1
+        f1 = f3
     
+num = int(input("Enter the number: "))
+fibbonacci(num)
