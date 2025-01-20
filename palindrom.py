4fbbc59eb324dff2de8182ddef650730aac92fe1
Abdul Sattar <asattar.bscsses21@iba-suk.edu.pk>
2025-01-20 14:40:01 +0500

    third commit

---
+++ palindrom.py
@@ -0,0 +1,11 @@
+
+
+def palindrome(s):
+    s = s.lower()
+    if s == s[::-1]:
+        print("string is palindrome")
+    else:
+        print("string is not palindrome")
+
+s1 = input("Enter the string: ")
+palindrome(s1)
